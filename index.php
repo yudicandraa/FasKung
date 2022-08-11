3770a42d8a0575b4e665ab8eec1ab49ccd8b003f
Yudi Candra <yudi.c@mhs.unsyiah.ac.id>
2022-08-11 11:41:12 +0700

    Logo

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
-<DOCTYPE
-<html lang="en">
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title> PLN UIW ACEH | FasKung </title>
+        <link rel="stylesheet" href="style.css">
+        <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Test</title>
     </head>
+
     <body>
         
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <div class="logo_content">
+            <div class="logo">
+                <div class="logo_name">PLN Unit Induk Wilayah | Aceh</div>
+            </div>
+        </div>
+    </div>
     </body>
-    
+
 </html>
